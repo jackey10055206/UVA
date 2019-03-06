df5a9402da8a2804e44e45e09ceb9e52b11b846a
jackey8869 <jackey10055206@gmail.com>
2019-03-06 17:00:04 +0800

    uva1121

---
+++ 1121/1121.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n ,s ;
+	while(cin >> n >> s)
+	{
+		vector<int> sequence;
+		for (int i = 0; i < n; ++i)
+		{
+			int num;
+			cin >> num;
+			sequence.push_back(num);
+		}
+
+
+		int sum = 0, minlength = n+1, max = 0;
+
+		for (int i = 0; i < n; ++i)
+		{
+			sum += sequence[i];
+
+			while(sum >= s)
+			{
+				minlength = min(minlength , i - max + 1);
+				sum -= sequence[max];
+				++max;
+			}
+		}
+
+		printf("%d\n", (minlength == n + 1)? 0:minlength);
+	}
+	return 0;
+}
